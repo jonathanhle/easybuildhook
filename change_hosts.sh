03d9e15c9b64b335616ed4d31d17c3e5f521ebd1
System Administrator <root@Hot-pickle.local>
2015-03-01 13:23:14 -0800

    Updated for with WP site IP.

--- change_hosts.sh
+++ change_hosts.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
+sudo killall -HUP mDNSResponder
+defaults write com.apple.safari WebKitDNSPrefetchingEnabled -boolean false
+sudo discoveryutil udnsflushcaches
+
 if cat /etc/hosts | grep netflix
   then
     #Remove Netflix lines from /etc/hosts 
     sed -i '' '/netflix/d' /etc/hosts
+    sudo discoveryutil mdnsflushcache
   else
     #Add lines for Netflix to /etc/hosts to block it
-    echo "127.0.0.1	netflix.com" >> /etc/hosts
-    echo "127.0.0.1 	www.netflix.com" >> /etc/hosts
+    echo "104.236.173.195	netflix.com" >> /etc/hosts
+    echo "104.236.173.195 	www.netflix.com" >> /etc/hosts
+    sudo discoveryutil mdnsflushcache
 fi
--- start_hook.sh
+++ start_hook.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+export NODE_PATH=$NODE_PATH:/usr/local/lib/node_modules/
 /usr/local/bin/node /var/root/moveit_node_server/index.js
